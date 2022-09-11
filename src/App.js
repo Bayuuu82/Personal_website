63b15c79284c080ef20aed3a0bd22bc13812f73b
bayuuu82 <bayok08022002@gmail.com>
2022-09-10 17:29:02 -0700

    ada sedikit perbaikan

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import './App.css';
-import Home from './page/Home/Home';
-import About from './page/About/About';
-import Profile from './page/Profile/Profile';
-import Skill from './page/Skill/Skill';
-import Navbar from './components/navigation/Navbar';
-import {Routes, Route} from 'react-router-dom'
+import "./App.css";
+import Home from "./page/Home/Home";
+import About from "./page/About/About";
+import Profile from "./page/Profile/Profile";
+import Skill from "./page/Skill/Skill";
+import Navbar from "./components/navigation/Navbar";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <Navbar/>
-    {/* <Home /> */}
-    {/* <About />
-    <Profile />
-    <Skill /> */}
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/About' element={<About />} />
-      <Route path='/Profile' element={<Profile />} />
-      <Route path='/Skill' element={<Skill />} />
-    </Routes>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Profile" element={<Profile />} />
+        <Route path="/Skill" element={<Skill />} />
+      </Routes>
     </>
   );
 }
--- src/components/navigation/Navbar.jsx
+++ src/components/navigation/Navbar.jsx
@@ -1,20 +1,27 @@
-import './navbar.css';
+import "./navbar.css";
 import { Link } from "react-router-dom";
 
 function Navbar() {
-    return (
-        <>
-        <div className="bar">
-            <ul>
-                <Link to='/'><li className='nav'>Home</li></Link>
-                <Link to='/About'><li className='nav'>About</li></Link>
-                <Link to='/Profile'><li className='nav'>Profile</li></Link>
-                <Link to='/Blog'><li className='nav'>Blog</li></Link>
-                <Link to='/Skill'><li className='nav'>Skill</li></Link>
-            </ul>
-        </div>
-        </>
-    )
+  return (
+    <>
+      <div className="bar">
+        <ul>
+          <Link to="/">
+            <li className="nav">Home</li>
+          </Link>
+          <Link to="/About">
+            <li className="nav">About</li>
+          </Link>
+          <Link to="/Profile">
+            <li className="nav">Profile</li>
+          </Link>
+          <Link to="/Skill">
+            <li className="nav">Skill</li>
+          </Link>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
